6be83d78b99c58b256b879f1ab2fecc0f933f973
Sosnowski <fsos@pl.impaq.corp>
2020-02-11 11:33:25 +0100

    Initial commit

---
+++ src/com/scala/oop/commands/Command.scala
@@ -0,0 +1,5 @@
+package com.scala.oop.commands
+
+trait Command {
+
+}
---
+++ src/com/scala/oop/filesystem/Filesystem.scala
@@ -0,0 +1,11 @@
+package com.scala.oop.filesystem
+
+import java.util.Scanner
+
+object Filesystem extends App {
+  val scanner = new Scanner(System.in)
+  while (true) {
+    print("$")
+    println(scanner.nextLine())
+  }
+}
